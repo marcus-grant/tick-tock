30d99a56a18974baf8de582c24f7dc04c4a7b089
Marcus Grant <marcusfg@gmail.com>
2018-06-19 16:13:31 -0400

    The boilerplate for webpack, react & sass finally worksgit status!

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './App';
+import App from './containers/App';
 
-import './styles/main.scss';
+import './main.scss';
 
 render(React.createElement(App), document.getElementById('react-root'));
