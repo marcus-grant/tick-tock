57cfa38031bbe71f8521e169b55ec73ccc04f06e
Marcus Grant <marcusfg@gmail.com>
2018-06-21 18:32:50 -0400

    groundwork components to add redux, refactor to use state first

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
 
 // import Counter from './components/Counter';
 import TimerContainer from './TimerContainer';
-import reducers from '../reducers/reducers';
+// import reducers from '../reducers/reducers';
 
 // import './styles/main.scss';
 
-const createStoreWithMiddleWare = applyMiddleware()(createStore);
-const store = createStoreWithMiddleWare(reducers);
+// const createStoreWithMiddleWare = applyMiddleware()(createStore);
+// const store = createStoreWithMiddleWare(reducers);
 
 const App = () => (
-  <Provider store={store}>
+  <div>
     <h1>Tick Tock</h1>
     <TimerContainer text="Test" />
-  </Provider>
+  </div>
 );
 
+  // <Provider store={store}>
+  //   <h1>Tick Tock</h1>
+  //   <TimerContainer text="Test" />
+  // </Provider>
 // until webpack is fixed use this placeholder
 export default App;
--- src/containers/TimerContainer.jsx
+++ src/containers/TimerContainer.jsx
@@ -1,36 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 
 // Todo, this should also be able contained inside a TimerListContainer to store ea. timer info...
 // time being counted shouldn't be stored in the list as some timers aren't being updated when stop
 
-@connect(store => ({
-  timeLeft: store,
-}))
+// @connect(store => ({
+//   timeLeft: store,
+// }))
 
 class TimerContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      secondsLeft: 100,
+    };
+  }
+  // tick = () => {
+  //   this.props.decrementTimerAction();
+  //   // this.props.incrementTimerAction();
+  // }
   tick = () => {
-    this.props.decrementTimerAction();
-    // this.props.incrementTimerAction();
+    this.setState(prevState => ({ secondsLeft: prevState.secondsLeft - 1 }));
+    // console.log('clicked');
   }
 
   render() {
-    const { timeLeft, text } = this.props;
+    const { text } = this.props;
     return (
       <div className="timer__container">
         <h2>{text}</h2>
-        <h1>{timeLeft}</h1>
+        <h1>{this.state.secondsLeft}</h1>
         <h4>Placeholder for TimerControls</h4>
+        <button onClick={this.tick}>tick</button>
       </div>
     );
   }
 }
 
 TimerContainer.propTypes = {
-  timeLeft: PropTypes.number.isRequired,
+  // timeLeft: PropTypes.number.isRequired, shouldn't this state reside here or be subbed here?
   // incrementTimerAction: PropTypes.func.isRequired,
-  decrementTimerAction: PropTypes.func.isRequired,
+  // decrementTimerAction: PropTypes.func.isRequired,
   text: PropTypes.string,
 };
 
