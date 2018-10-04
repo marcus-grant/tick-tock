0aabaabb35d8512a0318b1c88d0bd8ff3a2f94c3
Marcus Grant <marcusfg@gmail.com>
2018-10-04 11:54:56 -0400

    minor work towards event handlers

--- src/constants/action-types.js
+++ src/constants/action-types.js
@@ -3,6 +3,7 @@
 // Imported in Actions & Reducers
 // Prevents errors due to typos
 
+// Counters
 export const TICK_COUNTER = 'CNT_TCK';
 export const RESET_COUNTER = 'CNT_RST';
 export const SET_COUNTER_STOP = 'CNT_SET_STOP';
@@ -10,3 +11,6 @@ export const DEACTIVATE_COUNTER = 'CNT_DACT';
 export const ACTIVATE_COUNTER = 'CNT_ACT';
 export const ADD_COUNTER = 'CNT_ADD';
 export const REMOVE_COUNTER = 'CNT_DEL';
+
+// Events
+// export const 
